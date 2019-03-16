26d6adce897c7fe00cbebfd329fd19f0b5b22dbc
SamuelDeboni <samueldeboniss@gmail.com>
2019-03-16 14:17:27 -0300

    Added a lot of stuff

--- Main.gd
+++ Main.gd
@@ -1,20 +1,51 @@
 extends Node2D
 
-var connections = 0
+# Resources ---------------
+var connections = 0.0
 var influence_points = 0.0
-var influence_mult = 0.1
+
+
+var invite_multi = 1
+var invite_bot_multi = 0
+
+var invites = 0.0
+
+# Prices --------------------
+var invite_script_price = 10
+var invite_bot_price = 50
 
 func _ready():
-	pass 
+	$Control/Store/InviteScript.text = " Invite Script " + str(invite_script_price) + "IP" 
+	$Control/Store/InviteBot.text = " Auto Invite Bot " + str(invite_bot_price) + "IP"
+	
+	
 
 
 func _process(delta):
-	influence_points += delta*connections*influence_mult
+	influence_points += delta*connections*0.1
+	connections += invite_bot_multi * delta * 0.1
+	invites += connections * delta * 0.01
 	
-	$Control/ConnectionsT.text = "Connections " + str(connections)
-	$Control/InfluenceT.text = "Influence points " + str(influence_points)
-
+	$Control/ConnectionsT.text = "Connections " + str(int(connections)) + " " + str(int(invite_bot_multi) * 0.1) + "/sec"
+	$Control/InfluenceT.text = "Influence points " + str(int(influence_points)) + " " + str(int(connections)*0.1) + "/sec"
+	$Control/Invites.text = "Invites Recived " + str(int(invites))
 
 func _on_SendInvite_button_down():
 	if rand_range(0,10) > 9:
-		connections += 1
+		connections += 1 * invite_multi
+
+
+func _on_InviteScript_button_down():
+	if influence_points >= invite_script_price:
+		influence_points -= invite_script_price
+		invite_multi += 1
+		invite_script_price += 2
+	$Control/Store/InviteScript.text = str(invite_multi-1) + " Invite Script " + str(invite_script_price) + "IP" 
+
+
+func _on_InviteBot_button_down():
+	if influence_points >= invite_bot_price:
+		influence_points -= invite_bot_price
+		invite_bot_multi += 1
+		invite_bot_price += 5
+	$Control/Store/InviteBot.text = str(invite_bot_multi) + " Auto Invite Bot " + str(invite_bot_price) + "IP"
